cc9f66cbf6089179ee4c4885dc4127976735c2a1
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-05-30 15:13:36 -0300

    feat: viacep API has been implemented

---
+++ js/address.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const preencherFormalario = (address) => {
+    document.getElementById('address').value = address.logradouro;
+    document .getElementById('neighborhood').value = address.bairro;
+    document.getElementById('city').value = address.localidade;
+    document.getElementById('state').value = address.uf;
+}
+
+const pesquisarCep = async () => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const address = await dados.json();
+    preencherFormalario(address);
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout', pesquisarCep);
